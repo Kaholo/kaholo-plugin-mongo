9cd3f54c52824cb4e5c2439a42263ac391484d1c
xkcm <xkcm16@gmail.com>
2022-12-20 23:08:06 +0100

    Add linting

--- app.js
+++ app.js
@@ -1,248 +1,260 @@
-let MongoClient = require("mongodb").MongoClient;
-const child_process = require("child_process")
+const { MongoClient } = require("mongodb");
+const childProcess = require("child_process");
 
-function _queryHandler(query){
-    if (!query)
-        throw new Error("No query")
-    if (typeof query == 'string'){
-        try {
-            return JSON.parse(query);
-        } catch(e){
-            throw new Error("Error parsing query: " + e.message);
-        }
+function queryHandler(query) {
+  if (!query) {
+    throw new Error("No query");
+  }
+  if (typeof query === "string") {
+    try {
+      return JSON.parse(query);
+    } catch (e) {
+      throw new Error(`Error parsing query: ${e.message}`);
     }
-    return query;
+  }
+  return query;
 }
 
-function _mongooseCallback(db, resolve, reject){
-    return (err,data) => {
-        db.close();
+function mongooseCallback(db, resolve, reject) {
+  return (err, data) => {
+    db.close();
 
-        if (err)
-            return reject(err);
-        
-        resolve(data)
+    if (err) {
+      reject(err);
     }
+
+    return resolve(data);
+  };
 }
 
-function _execMongo(action){
-    return new Promise((resolve, reject)=>{
-        MongoClient.connect(action.params.URL, (err, db) => {
-            if (err) {
-                return reject("Error connecting to DB");
-            }
-            
-            let collection = db.collection(action.params.COLLECTION);
-            resolve({
-                collection : collection,
-                db : db
-            });
-        })
-    })
+function execMongo(action) {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(action.params.URL, (err, db) => {
+      if (err) {
+        reject(new Error("Error connecting to DB"));
+      }
+
+      const collection = db.collection(action.params.COLLECTION);
+      return resolve({
+        collection,
+        db,
+      });
+    });
+  });
 }
 
 function find(action) {
-    return new Promise((resolve, reject) => {
-        let query;
-        if (!action.params.COLLECTION)
-            return reject({ "err": "No collection" });
-        
-        try {
-            query = _queryHandler(action.params.QUERY);
-        } catch (e) {
-            console.log(e);
-            return reject({ "err": e });
-        }
+  return new Promise((resolve, reject) => {
+    let query;
+    if (!action.params.COLLECTION) {
+      reject(new Error("No collection"));
+    }
 
-        _execMongo(action).then(res=>{
-            res.collection.find(query).toArray(_mongooseCallback(res.db, resolve, reject));                
-        }).catch(reject)
-    });
+    try {
+      query = queryHandler(action.params.QUERY);
+    } catch (e) {
+      console.error(e);
+      reject(e);
+    }
+
+    execMongo(action).then((res) => {
+      res.collection.find(query).toArray(mongooseCallback(res.db, resolve, reject));
+    }).catch(reject);
+  });
 }
 
 function findOne(action) {
-    return new Promise((resolve, reject) => {
-        let query;
-        if (!action.params.COLLECTION)
-            return reject({ "err": "No collection" });
-        
-        try {
-            query = _queryHandler(action.params.QUERY);
-        } catch (e) {
-            console.log(e);
-            return reject({ "err": e });
-        }
+  return new Promise((resolve, reject) => {
+    let query;
+    if (!action.params.COLLECTION) {
+      reject(new Error("No collection"));
+    }
 
-        _execMongo(action).then(res=>{
-            res.collection.findOne(query, _mongooseCallback(res.db, resolve, reject));
-        }).catch(reject)
-    });
+    try {
+      query = queryHandler(action.params.QUERY);
+    } catch (e) {
+      console.error(e);
+      reject(e);
+    }
+
+    execMongo(action).then((res) => {
+      res.collection.findOne(query, mongooseCallback(res.db, resolve, reject));
+    }).catch(reject);
+  });
 }
 
 function deleteOne(action) {
-    return new Promise((resolve, reject) => {
-        if (!action.params.QUERY)
-            return reject({ "err": "No query" });
-        if (!action.params.COLLECTION)
-            return reject({ "err": "No collection" });
-        
-        let query;
-        try {
-            query = _queryHandler(action.params.QUERY);
-        } catch (e) {
-            console.log(e);
-            return reject({ "err": e });
-        }
+  return new Promise((resolve, reject) => {
+    if (!action.params.QUERY) {
+      reject(new Error("No query"));
+    }
+    if (!action.params.COLLECTION) {
+      reject(new Error("No collection"));
+    }
 
-        _execMongo(action).then(res=>{
-            res.collection.deleteOne(query, _mongooseCallback(res.db, resolve, reject));
-        }).catch(reject)
-    });
+    let query;
+    try {
+      query = queryHandler(action.params.QUERY);
+    } catch (e) {
+      console.error(e);
+      reject(e);
+    }
+
+    execMongo(action).then((res) => {
+      res.collection.deleteOne(query, mongooseCallback(res.db, resolve, reject));
+    }).catch(reject);
+  });
 }
 
 function deleteMany(action) {
-    return new Promise((resolve, reject) => {
-        if (!action.params.COLLECTION)
-            return reject({ "err": "No collection" });
-        
-        let query;
-        try {
-            query = _queryHandler(action.params.QUERY);
-        } catch (e) {
-            console.log(e);
-            return reject({ "err": e });
-        }
+  return new Promise((resolve, reject) => {
+    if (!action.params.COLLECTION) {
+      reject(new Error("No collection"));
+    }
 
-        _execMongo(action).then(res=>{
-            res.collection.deleteMany(query, _mongooseCallback(res.db, resolve, reject));
-        }).catch(reject)
-    });
+    let query;
+    try {
+      query = queryHandler(action.params.QUERY);
+    } catch (e) {
+      console.error(e);
+      reject(e);
+    }
+
+    execMongo(action).then((res) => {
+      res.collection.deleteMany(query, mongooseCallback(res.db, resolve, reject));
+    }).catch(reject);
+  });
 }
 
 function updateOne(action) {
-    return new Promise((resolve, reject) => {
-        if (!action.params.COLLECTION)
-            return reject({ "err": "No collection" });
-        if (!action.params.UPDATE)
-            return reject({ "err": "No update value" });
+  return new Promise((resolve, reject) => {
+    if (!action.params.COLLECTION) {
+      reject(new Error("No collection"));
+    }
+    if (!action.params.UPDATE) {
+      reject(new Error("No update value"));
+    }
 
-        let query;
-        try {
-            query = _queryHandler(action.params.QUERY);
-        } catch (e) {
-            console.log(e);
-            return reject({ "err": e });
-        }
+    let query;
+    try {
+      query = queryHandler(action.params.QUERY);
+    } catch (e) {
+      console.error(e);
+      reject(e);
+    }
 
-        let update;
-        try {
-            update = JSON.parse(action.params.UPDATE);
-        } catch (e) {
-            console.log("Error parsing update query: ", e);
-            reject({ "err": e });
-        }
+    let update;
+    try {
+      update = JSON.parse(action.params.UPDATE);
+    } catch (e) {
+      console.error("Error parsing update query: ", e);
+      reject(e);
+    }
 
-        _execMongo(action).then(res=>{
-            res.collection.updateOne(query, update, _mongooseCallback(res.db, resolve, reject));
-        }).catch(reject)
-    });
+    execMongo(action).then((res) => {
+      res.collection.updateOne(query, update, mongooseCallback(res.db, resolve, reject));
+    }).catch(reject);
+  });
 }
 
 function updateMany(action) {
-    return new Promise((resolve, reject) => {
-        if (!action.params.COLLECTION)
-            return reject({ "err": "No collection" });
-        
-        let query;
-        try {
-            query = _queryHandler(action.params.QUERY);
-        } catch (e) {
-            console.log(e);
-            return reject({ "err": e });
-        }
-        
-        let update;
-        try {
-            update = JSON.parse(action.params.UPDATE);
-        } catch (e) {
-            console.log("Error parsing update query: ", e);
-            reject({ "err": e });
-        }
+  return new Promise((resolve, reject) => {
+    if (!action.params.COLLECTION) {
+      reject(new Error("No collection"));
+    }
 
-        _execMongo(action).then(res=>{
-            res.collection.updateMany(query, update, _mongooseCallback(res.db, resolve, reject));
-        }).catch(reject)
-    });
+    let query;
+    try {
+      query = queryHandler(action.params.QUERY);
+    } catch (e) {
+      console.error(e);
+      reject(e);
+    }
+
+    let update;
+    try {
+      update = JSON.parse(action.params.UPDATE);
+    } catch (e) {
+      console.error("Error parsing update query: ", e);
+      reject(e);
+    }
+
+    execMongo(action).then((res) => {
+      res.collection.updateMany(query, update, mongooseCallback(res.db, resolve, reject));
+    }).catch(reject);
+  });
 }
 
 function insert(action) {
-    return new Promise((resolve, reject) => {
-        if (!action.params.COLLECTION)
-            return reject({ "err": "No collection" });
-        if (!action.params.DOCUMENT)
-            reject({ "err": "No document" })
-        let parsedDoc;
-        try {
-            parsedDoc = JSON.parse(action.params.DOCUMENT);
-        } catch (e) {
-            console.log("Error parsing document: ", e);
-            reject({ "err": e });
-        }
+  return new Promise((resolve, reject) => {
+    if (!action.params.COLLECTION) {
+      reject(new Error("No collection"));
+    }
+    if (!action.params.DOCUMENT) {
+      reject(new Error("No document"));
+    }
+    let parsedDoc;
+    try {
+      parsedDoc = JSON.parse(action.params.DOCUMENT);
+    } catch (e) {
+      console.error("Error parsing document: ", e);
+      reject(e);
+    }
 
-        _execMongo(action).then(res=>{
-            res.collection.insertOne(parsedDoc, _mongooseCallback(res.db, resolve, reject));
-        }).catch(reject);
-    });
+    execMongo(action).then((res) => {
+      res.collection.insertOne(parsedDoc, mongooseCallback(res.db, resolve, reject));
+    }).catch(reject);
+  });
 }
 
 function insertMany(action) {
-    return new Promise((resolve, reject) => {
-        if (!action.params.DOCUMENTS)
-            reject({ "err": "No document" })
-        let parsedDoc;
-        try {
-            parsedDoc = JSON.parse(action.params.DOCUMENTS);
-        } catch (e) {
-            console.log("Error parsing document: ", e);
-            reject({ "err": e });
-        }
+  return new Promise((resolve, reject) => {
+    if (!action.params.DOCUMENTS) {
+      reject(new Error("No document"));
+    }
+    let parsedDoc;
+    try {
+      parsedDoc = JSON.parse(action.params.DOCUMENTS);
+    } catch (e) {
+      console.error("Error parsing document: ", e);
+      reject(e);
+    }
 
-        _execMongo(action).then(res=>{
-            res.collection.insertMany(parsedDoc, _mongooseCallback(res.db, resolve, reject));
-        }).catch(reject);
+    execMongo(action).then((res) => {
+      res.collection.insertMany(parsedDoc, mongooseCallback(res.db, resolve, reject));
+    }).catch(reject);
+  });
+}
+
+function mongodump(action) {
+  const url = action.params.URL;
+  const params = action.params.OTHER;
+  const path = action.params.OUT;
+  const cmd = `mongodump --uri=${url} -o ${path} ${params}`;
+  return new Promise((resolve, reject) => {
+    console.info(`executing:${cmd}`);
+    childProcess.exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        reject(new Error(`exec error: ${error}`));
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        reject(new Error(`exec error: ${stderr}`));
+      }
+      console.info(stdout);
+      return resolve(stdout);
     });
+  });
 }
-
-function mongodump (action) {
-    const url = action.params.URL;
-    const params = action.params.OTHER;
-    const path = action.params.OUT;
-    let cmd = `mongodump --uri=${url} -o ${path} ${params}`;
-    return new Promise((resolve, reject) => {
-        console.log("executing:" + cmd)
-        child_process.exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                return reject(`exec error: ${error}`);
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return reject(`exec error: ${stderr}`);
-            }
-            console.log(stdout)
-            return resolve(stdout);
-        });
-    })
-}
-
 
 module.exports = {
-    find: find,
-    findOne: findOne,
-    insertOne: insert,
-    insertMany: insertMany,
-    deleteOne: deleteOne,
-    deleteMany: deleteMany,
-    updateOne: updateOne,
-    updateMany: updateMany,
-    mongodump: mongodump
+  find,
+  findOne,
+  insertOne: insert,
+  insertMany,
+  deleteOne,
+  deleteMany,
+  updateOne,
+  updateMany,
+  mongodump,
 };
